de0a665825b9b2f969e2211116c42c577a58cb7f
Serafin Bernales <raffibernales@gmail.com>
2025-12-10 12:37:33 +0800

    Prepare project for GitHub and deployment

--- my_portfolio/settings.py
+++ my_portfolio/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2wef(!e!q(7gzsn*_kld7e20h2_m%69lcrjd9cl_nvo6bewb12'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -118,3 +118,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+import os
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
